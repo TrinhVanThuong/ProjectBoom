4a34245de0ea384a67d26932f19df4fe15750c39
TrinhVanThuong <anhtomthuong99@gmail.com>
2019-11-21 20:08:45 +0700

    new1

---
+++ BoomT/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+     void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 currenVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+    }
+ 
+}
